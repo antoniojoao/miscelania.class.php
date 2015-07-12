87112e9b1caf24cc7477c4da980647e3e5616fe4
antoniojoao <jo12_cruzeiro@hotmail.com>
2015-07-12 17:49:49 -0300

    Update miscellania.class.php

--- miscellania.class.php
+++ miscellania.class.php
@@ -1,5 +1,175 @@
 <?php
 class Miscellania {
+  public $pais;
+ public function pais($pais){
+ if ($pais=="frança") {
+  
+  echo"idioma é françês";
+ }
+if($pais=="Albânia") {
+  
+  echo"idioma é albanês";
+ }
+if($pais=="Alemanha") {
+  
+  echo"idioma é alemão";
+ }
+if($pais=="Andorra") {
+  
+  echo"idioma é catalão";
+ }
+if($pais=="Áustria") {
+  
+  echo"idioma é alemão";
+ }
+if($pais=="Belarus") {
+  
+  echo"idioma é bielo-russo e russo";
+ }
+ if($pais=="Bélgica") {
+  
+  echo"Francês e Alemão ";
+ }
+ if($pais=="Bósnia e Herzegóvina ") {
+  
+  echo"Bósnio,Croata,Servio";
+ }
+ if($pais=="Bulgária") {
+  
+  echo"Bulgaro";
+ }
+ if($pais=="Croácia") {
+  
+  echo"idioma é Coatra";
+ }
+ 
+ if($pais=="Dinamarca") {
+  
+  echo"idioma é o dinamarquês";
+ }
+ if($pais=="Eslováquia") {
+  
+  echo"idioma é bielo-russo e russo";
+ }
+ if($pais=="Eslovênia") {
+  
+  echo"idioma é eslovaco";
+ }
+ if($pais=="Espanha") {
+  
+  echo"idioma é  castelhano";
+ }
+ if($pais=="Estônia ") {
+  
+  echo"idioma é  estoniano";
+ }
+ if($pais=="Federação Russa") {
+  
+  echo"idioma é russo";
+ }
+ if($pais=="Finlândia") {
+  
+  echo" é um idioma da família de línguas urálicas do grupo fino-permiano, ao qual também pertencem o húngaro e o estoniano.";
+ }
+ 
+ if($pais=="Grécia") {
+  
+  echo"idioma é grego";
+ }
+ if($pais=="Holanda") {
+  
+  echo"idioma é inglẽs,alemão e francês";
+ }
+ if($pais=="Hungria") {
+  
+  echo"húngaro";
+ }
+ if($pais=="Itália") {
+  
+  echo"italiano";
+ }
+ if($pais=="Letônia") {
+  
+  echo"idioma é letão";
+ }
+ if($pais=="Liechtenstein") {
+  
+  echo"idioma é alemão";
+ }
+ if($pais=="Lituânia") {
+  
+  echo"idioma é lituano";
+ }
+ if($pais=="Luxemburgo") {
+  
+  echo" luxemburguês";
+ }
+ if($pais=="Macedônia ") {
+  
+  echo"macedónio ";
+ }
+ if($pais=="Moldávia") {
+  
+  echo"moldávio,russo e romeno";
+ }
+ if($pais=="Mônaco ") {
+  
+  echo"francês (oficial), inglês, italiano e monegasco.";
+ }
+ if($pais=="Montenegro") {
+  
+  echo"idioma é sérvio, bósnio, albanês e o croata ";
+ }
+ if($pais=="Noruega") {
+  
+  echo"idioma é norueguês";
+ }
+ if($pais=="Polônia") {
+  
+  echo"idioma é polonês";
+ }
+ if($pais=="Portugal") {
+  
+  echo"idioma é portugues ";
+ }
+ if($pais=="República Tcheca ") {
+  
+  echo"idioma é Theco";
+ }
+ if($pais=="Romênia") {
+  
+  echo" romeno ";
+ }
+ if($pais=="San Marino ") {
+  
+  echo"idioma é italiano";
+ }
+ if($pais=="Sérvia ") {
+  
+  echo"idioma é  Bósnia e Herzegovina";
+ }
+ 
+ 
+ if($pais=="Suécia") {
+  
+  echo"idioma é sueco";
+ }
+ 
+ if($pais=="Turquia ") {
+  
+  echo"idioma é Turco";
+ }
+ 
+ if($pais=="Ucrânia") {
+  
+  echo"idioma é ucraniano";
+ }
+ if($pais=="Vaticano") {
+  
+  echo"italiano e o latim";
+ }
+
+}
 
 }
 ?>
